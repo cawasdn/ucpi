312b5a01e3986c301fc2bd8c1f6cc605a2b139fa
Daniel <cawasdn@gmail.com>
2025-04-12 16:03:32 +0800

    Section and footer added

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navigation from "./components/Navigation";
 import Services from "./components/Services";
@@ -8,6 +9,7 @@ export default function Home() {
       <Navigation />
       <Hero />
       <Services />
+      <Footer />
     </div>
   );
 }
